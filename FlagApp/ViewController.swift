5c3dc1b28fb6501ed7636f8676e92bf9cfd2bddd
Emanuel Schmoczer <emanuel@schmoczer.com>
2019-06-21 15:01:24 +0200

    initial commit

---
+++ FlagApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  FlagApp
+//
+//  Created by Emanuel Schmoczer on 21.06.19.
+//  Copyright © 2019 Emanuel Schmoczer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let countryCodes = NSLocale.isoCountryCodes
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
